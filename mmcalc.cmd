c580b1993b91889b939a06acb9ba0380921b1c7a
statto <statto@dbcac6b5-a2bf-4854-81b0-ca02f29e692d>
2011-02-10 14:30:55 +0000

    Now pauses if MmCalc throws an error, allowing the error message to be read if launched by double-clicking.

--- mmcalc.cmd
+++ mmcalc.cmd
@@ -2,4 +2,5 @@
 @ECHO off
 TITLE MmCalc (%CD%)
 python mmcalc.py
-if errorlevel 1 (ECHO MmCalc error! If you can reproduce this issue, please send the error messages along with a description of how to cause this problem to mmcalc@andrewsteele.co.uk)
+if errorlevel 1 (ECHO MmCalc error! If you can reproduce this issue, please send the error messages along with a description of how to cause this problem to mmcalc@andrewsteele.co.uk
+PAUSE)
